4c231734fd078ac6a4d8c1186cfcbcdfd0546e66
Kohei Hayashi <19472b185f@gmail.com>
2022-03-31 23:17:46 +0900

    init

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = "Hello World!!!"
-    return name
+def hello_world():
+    return "Hello World!!!!"
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
+@app.route('/hello', methods=['POST'])
+def hello():
+    if request.method=='POST':
+        name = request.form['name']
+    else:
+        name = 'no name.'
+    return render_template('hello.html', title='flask test', name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
